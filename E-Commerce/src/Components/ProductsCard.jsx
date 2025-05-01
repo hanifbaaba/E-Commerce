e8ae08226b0b769d4d4d19e899491f34c221838f
hanifubaaba <baabahanif88@gmail.com>
2025-05-01 13:20:01 +0100

    added picture placeholder

---
+++ E-Commerce/src/Components/ProductsCard.jsx
@@ -0,0 +1,9 @@
+export default function ProductsCard({ name, price, image }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={image} alt="" className="images" />
+      <p>${price}</p>
+    </div>
+  );
+}
--- E-Commerce/src/Pages/ProductsPage.jsx
+++ E-Commerce/src/Pages/ProductsPage.jsx
@@ -1,7 +1,40 @@
+import ProductsCard from "../Components/ProductsCard";
+
 export default function ProductsPage() {
+  const products = [
+    {
+      id: 1,
+      name: "T-Shirt",
+      price: 20,
+      image: "/images/tshirtImg.jpg",
+    },
+    {
+      id: 2,
+      name: "Sneakers",
+      price: 50,
+      image: "/images/sneakersImg.jpg",
+    },
+    {
+      id: 3,
+      name: "Hat",
+      price: 15,
+      image: "/images/hatImg.jpg",
+    },
+  ];
+
   return (
     <div>
-      <h1>Explore Our Products</h1>
+      <h1>Products</h1>
+      <div className="products-grid">
+        {products.map((item) => (
+          <ProductsCard
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
+      </div>
     </div>
   );
 }
